1d646ddd5227de43a91c19ec1ca51e1343812757
R1nge <r1ngegamedev@gmail.com>
2023-11-02 16:01:24 +0300

    Renamed 'Player' -> 'Unit'

---
+++ Assets/Scripts/Players/UnitMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Players
+{
+    public class UnitMovement
+    {
+        private readonly NavMeshAgent _navMeshAgent;
+        
+        public UnitMovement(NavMeshAgent navMeshAgent) => _navMeshAgent = navMeshAgent;
+
+        public void MoveTo(Vector3 position) => _navMeshAgent.SetDestination(position);
+    }
+}
